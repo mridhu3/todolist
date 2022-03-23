b3fd554201372c7160e919c1b0c4f4d7f802441c
mridhu3 <76735650+mridhu3@users.noreply.github.com>
2022-03-23 19:28:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="Todo-app">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
